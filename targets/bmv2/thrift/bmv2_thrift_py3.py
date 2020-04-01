25adf71a65a8085cdf058596012d85326eb6abd8
Ralf Kundel <ralf.kundel@kom.tu-darmstadt.de>
2020-04-01 13:15:42 +0200

    License update

--- targets/bmv2/thrift/bmv2_thrift_py3.py
+++ targets/bmv2/thrift/bmv2_thrift_py3.py
@@ -1,4 +1,6 @@
 # Copyright 2020-present Ralf Kundel, Fridolin Siegmund
+# Copyright 2013-present Barefoot Networks, Inc.
+# Antonin Bas (antonin@barefootnetworks.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
